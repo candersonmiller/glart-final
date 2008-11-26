4c5b7fb0952aa4cabee9d0bd7c6b78a59b24f5ff
C. Anderson Miller <candersonmiller@gmail.com>
2008-11-25 20:37:10 -0500

    committing getImageUrls function that gets image locations from a url

--- gethistory.py
+++ gethistory.py
@@ -9,6 +9,21 @@ from BeautifulSoup import BeautifulSoup
 from BeautifulSoup import SoupStrainer
 
 
+def getImageUrls(url):
+	request = urllib2.Request(url)
+	request.add_header('User-Agent', 'Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10.5; en-US; rv:1.9.0.4) Gecko/2008102920 Firefox/3.0.4')
+	opener = urllib2.build_opener()
+	feeddata = opener.open(request).read()
+	soup = BeautifulSoup(''.join(feeddata))
+	soup.prettify()
+	images = soup.findAll('img')
+	imageurls = list()
+	for image in images:
+		imageurl = image['src']
+		if( not (imageurl.count('magnify-clip') or imageurl.count('wikimedia-button') or imageurl.count('Commons-logo') or imageurl.count('Status_iucn') or imageurl.count('red_question_mark') or imageurl.count('Wikispecies') or imageurl.count('poweredby_mediawiki'))):
+			imageurls.append(imageurl)
+	return imageurls
+
 def main():
 	conn = MySQLdb.connect( host = "ec2-75-101-245-127.compute-1.amazonaws.com",
 							user = "wikihole",
@@ -22,12 +37,11 @@ def main():
 			break
 		print "%s\t%s\t%s" % (row[0], row[2], row[1])	
 		
-		#url = row[1]
-		#page = urllib2.urlopen(url)
-		#pageString = page.read()
-		#soup = BeautifulSoup(''.join(pageString))
-		#soup.renderContents()
-		#soup.prettify()
+		url = row[1]
+		imageurls = getImageUrls(url)
+		for imageurl in imageurls:
+			print imageurl
+		
 	
 if __name__ == '__main__':
 	main()
