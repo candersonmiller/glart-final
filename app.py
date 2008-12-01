652d7c42a23c5c1f2849ace3d44e4f71e59cce38
Thomas Gerhardt <Tom@Macintosh-8.local>
2008-12-01 12:18:29 -0500

    added universe class and app

---
+++ app.py
@@ -0,0 +1,235 @@
+#! /usr/bin/env python
+'''SelectRenderMode demonstration code with background thread
+
+Demonstrates use of "named transforms", objects which
+use push/pop of the name-stack to report selection during
+the rendermode.SelectRenderMode pass.
+
+Background thread perturbs the object positions.
+'''
+from OpenGLContext import testingcontext
+BaseContext, MainFunction = testingcontext.getInteractive(("glut",))
+from OpenGLContext import drawcube, context, interactivecontext
+from OpenGL.GL import *
+from OpenGL.GLU import *
+from OpenGL.GLUT import *
+from OpenGLContext.arrays import *
+import string, time, random, threading, random
+from OpenGLContext.scenegraph import basenodes
+from OpenGLContext import glutinteractivecontext
+from OpenGLContext import trackball
+from OpenGLContext.events.timer import Timer
+from OpenGLContext.scenegraph.text import toolsfont, fontprovider, fontstyle3d
+from OpenGLContext import displaylist
+
+import math
+import universe
+
+#from OpenGLContext import trackball, quaternion
+
+class TestContext( BaseContext ):
+	def OnInit( self ):
+		
+		self.stringArray = []
+		self.stringArray.append( "thi is a sweet string\ndon't you think?")
+		self.stringArray.append( "man o man this is \na long one")
+		self.stringArray.append( "poopty pants")
+		self.strPos = 0
+		
+		glutReshapeWindow( 800, 600)
+		glutPositionWindow( 0, 0 )
+		
+		self.time = Timer( duration = 60.0, repeating = 1 )
+		self.time.addEventHandler( "fraction", self.OnTimerFraction )
+		self.time.register (self)
+		self.time.start ()
+		
+		self.rot = 0
+		
+		#dragging flag
+		self.startDrag = 0
+		
+		#holder for last detail rendered planet
+		self.lastDetail = ()
+		
+		
+		#make some random planets and systems
+		self.universe = universe.Universe()
+		
+		numSys = 8#random.random()*100
+		for s in range( int(numSys) ):
+			self.universe.addSys( s )
+			numPlan = 10#random.random()*100+1
+			for p in range( int(numPlan) ):
+				numMoons = random.random()*4+1
+				self.universe.addPlanet( p,int(numMoons) )
+
+						
+		"""Setup callbacks and build geometry for rendering"""
+		#on mouse down
+		self.addEventHandler( "mousebutton", button = 0, state=1, function = self.mouseDown )
+		#on mouse up
+		self.addEventHandler( "mousebutton", button = 0, state = 0, function = self.mouseUp )
+				
+		glutinteractivecontext.GLUTInteractiveContext.setupDefaultEventCallbacks(self)
+		
+		self.initialPosition = ( 0,0,20 )
+		self.STEPDISTANCE = 5.0
+		self.newPos = self.initialPosition
+		self.goTo = 0
+		
+		#get fonts
+		providers = fontprovider.getProviders( 'solid' )
+		if not providers:
+			raise ImportError( """NONONO solid font providers registered! Demo won't function properly!""" )
+		registry = self.getTTFFiles()
+		styles = []
+		for font in registry.familyMembers( 'SANS' ):
+			names = registry.fontMembers( font, 400, 0)
+			for name in names:
+				styles.append( fontstyle3d.FontStyle3D(
+					family = [name],
+					size = .06,
+					justify = "LEFT",
+					thickness = .02,
+					quality = 3,
+					renderSides = 1,
+					renderFront = 1,
+					renderBack = 1,
+				))
+		self.styles = styles
+				
+		#r = threading.Thread( target = self.randomiser )
+		#r.setDaemon(1)
+		#r.start()
+		
+
+	def OnIdle( self, ):
+		if self.goTo:
+			self.tweenCam()
+		self.triggerRedraw(1)
+		return 1
+		
+	def getSceneGraph( self ):
+		return self.universe.uni
+		
+	def glutOnMouseMove(self, x, y):
+		if self.startDrag == 1:
+			trackP,trackQ = self.track.update( x,y )
+			print "p: ",trackP
+			print "q: ",trackQ
+			self.platform.setPosition( trackP )
+			self.platform.setOrientation( trackQ )
+			
+	def mouseDown( self, event ):
+		#make trackball
+		x,y  = event.getPickPoint()
+		print "track centerY: ", self.newPos[1]
+		self.track = trackball.Trackball( position=self.platform.position, quaternion=self.platform.quaternion, center=(self.newPos[0],self.newPos[1],self.newPos[2]-2), originalX=x, originalY=y, width=800, height=600 )
+		self.startDrag = 1
+		
+	def mouseUp( self, event ):
+		"""Handle a mouse-click in our window.
+		Retrieves the "pick point", and the unprojected (world-space) coordinates
+		of the clicked geometry (if a named object was clicked).
+		"""
+		
+		self.startDrag = 0
+		self.track.cancel()
+		
+		x,y  = event.getPickPoint()
+		print 'Click', (x,y)
+		
+		
+		print '  %s objects:'%( len(event.getObjectPaths()))
+		for path in event.getObjectPaths():
+			#find planet
+			foundPlanet = self.universe.findPlanet( path )
+			#find solarsys
+			foundSys = self.universe.findSys( path )
+			
+			
+			if foundPlanet and foundSys:
+				print "found planet:",foundPlanet.name, "in sys:", foundSys.name
+				#if there is one rendered detailed... put the simple back in
+				if self.lastDetail:
+					self.universe.unRenderDetail( self.lastDetail )
+					
+				#render font geom
+				self.geometry = basenodes.Text( fontStyle=self.styles[0], string=self.stringArray[ self.strPos ] )
+				self.strPos += 1
+					
+				#render the new one detailed
+				self.lastDetail = foundPlanet
+				self.universe.renderDetail( foundPlanet, self.geometry )
+				
+				
+				#calc the new point in global space
+				summedTrans =  foundPlanet.translation + foundSys.translation
+				self.newPos = ( summedTrans[0], summedTrans[1], summedTrans[2]+2 )
+				self.goTo = 1
+								
+								
+	def OnClick2( self, event ):
+		"""Handle mouse click for a given name/id"""
+		print "You clicked on the magic sphere!"
+		self.OnClick1( event )
+
+
+	def setupFontProviders( self ):
+		"""Load font providers for the context
+
+		See the OpenGLContext.scenegraph.text package for the
+		available font providers.
+		"""
+		fontprovider.setTTFRegistry(
+			self.getTTFFiles(),
+		)
+	
+	def randomiser( self ):
+		while self:
+			self.lockScenegraph()
+			try:
+				#rotate all the planets
+				#self.universe.rotatePlanets( self.rot )
+				self.rot += 0.05
+			finally:
+				self.unlockScenegraph()
+			if self:
+				self.triggerRedraw(1)
+			time.sleep( .05 )	
+			
+	def OnTimerFraction( self, event ):
+		jim = 2
+		self.universe.rotatePlanets( 60*event.fraction() )
+		
+	def tweenCam( self ):
+		speed = 0.1
+		#move to newPos
+		deltaVector = ( self.newPos[0]-self.platform.position[0] , self.newPos[1]-self.platform.position[1], self.newPos[2]-self.platform.position[2] )
+		deltaInc = ( deltaVector[0]*speed,deltaVector[1]*speed,deltaVector[2]*speed )
+		self.platform.moveRelative( x=deltaInc[0],y=deltaInc[1],z=deltaInc[2] )
+	
+		#look at newPos
+		#set orientation back to initial default 
+		self.platform.setOrientation( (0,1,0,0) )
+		#calc dela ang about y axis
+		dx = self.platform.position[0] - self.newPos[0]
+		dy = self.platform.position[1] - self.newPos[1]
+		dz = self.platform.position[2] - self.newPos[2]
+		#compute axis rotation angles
+		yAng = math.atan( dx/dz )
+		xAng = math.atan( dy/dz )
+		#apply turn on y axis
+		self.platform.turn( (0,1,0,yAng*speed) )
+		#apply turn to x axis
+		self.platform.turn( (1,0,0,-xAng*speed) )
+		
+		avgDist = math.fabs(deltaInc[0]+deltaInc[1]+deltaInc[2])/3
+		if( avgDist < 0.001 ):
+			self.goTo = 0
+			
+	
+
+if __name__ == "__main__":
+	MainFunction ( TestContext)
---
+++ universe.py
@@ -0,0 +1,248 @@
+#! /usr/bin/env python
+from OpenGLContext.scenegraph import basenodes, indexedlineset
+import math, random
+
+from OpenGLContext import displaylist
+from OpenGLContext.scenegraph.text import toolsfont
+
+
+
+class Universe( object ):
+	
+	def __init__( self ):
+		#create scene graph (universe)
+		self.uni = basenodes.sceneGraph()
+		#create one child for the universe... a galaxy
+		tmpGalaxy = []
+		t = basenodes.Transform(
+			translation = (0,0,0),
+			children = [],
+			)
+		t.name = "galaxy01"			
+		tmpGalaxy.append(t)
+		self.uni.children = tmpGalaxy #now one galaxy added to the universe... access it via self.uni.children[0]		
+				
+		
+	def addSys( self, name ):
+		"""adds solar system (session) to scenegraph"""		
+		coords = []
+		#transform to hold connect line
+		l = basenodes.Transform(
+			children = [
+				basenodes.Shape(
+					geometry = basenodes.IndexedLineSet(	
+						coord = basenodes.Coordinate(
+							point = coords,
+						),
+						coordIndex = range( len(coords)),
+					),
+					appearance = basenodes.Appearance(
+						material = basenodes.Material( emissiveColor = (0.5,0.5,0.5) ),
+					),
+				),
+			],
+		)
+		l.name = "line"
+		#transform to hold solar system
+		p = random.random()*44-22, random.random()*12-6, random.random()*44-22 #position
+		r = random.random()*math.radians(180) #rotation
+		t = basenodes.Transform(
+			translation = p,
+			children = [ l ],
+		)
+		t.name = name
+		t.sensititive = 0
+		#add solar system to galaxy
+		tmpHolder = self.uni.children[0].children
+		tmpHolder.append(t)
+		self.uni.children[0].children = tmpHolder
+		
+		
+	def addPlanet( self, name, moonsNum ):
+		"""adds planet (webpage) to last solar system"""
+		#make moon children transform
+		moonChildren = []
+		for m in range( moonsNum ):
+			#make the translation Transform for the moon and add it to the rotation Transform
+			orbitD = random.random()*.5+.2
+			mTrans = basenodes.Transform(
+				translation = ( orbitD,0,0 ),
+				rotation = ( 0,0,0,0 ),
+				children = [
+					basenodes.Shape(
+						geometry = basenodes.Box( size = ( 0.05,0.2,0.2) ),
+						appearance = basenodes.Appearance(
+							material = basenodes.Material( emissiveColor = (.8,.8,.8) ),
+						),
+					),
+				],
+			)
+			#make the rotation Transforms for the moon
+			mRatio = random.random()*0.5
+			mRotY = basenodes.Transform(
+				translation = (0,0,0 ),
+				rotation = (0,1,0,0),
+				children = [ mTrans ],
+			)
+			mr = random.random()*math.radians(360) #rotation
+			mRotX = basenodes.Transform(
+				translation = (0,0,0 ),
+				rotation = (mRatio,0,0,mr),
+				children = [ mRotY ],
+			)
+			speedMultiplyer = random.random()*2.5+0.5
+			mRotX.name = speedMultiplyer
+			moonChildren.append( mRotX )
+			
+		#make moons Transform
+		moons = basenodes.Transform(
+			translation = ( 0,0,0 ),
+			rotation = ( 0,0,0,0 ),
+			children = moonChildren
+		)
+			
+		#make planet Transform
+		rad = random.random()*0.4+0.2 #size
+		#c = random.random(), random.random(), random.random() #color
+		plan = basenodes.Transform(
+			translation = ( 0,0,0 ),
+			children = [
+				basenodes.TouchSensor(),
+				basenodes.Shape(
+					geometry = basenodes.Box( size = (rad,rad,rad) ),
+					appearance = basenodes.Appearance(
+						material = basenodes.Material( emissiveColor = (.8,.8,.8) ),
+					),
+				),
+			]
+		)		
+		
+		#transform to hold new planet and moons
+		p = random.random()*16-8, random.random()*16-8, random.random()*16-8 #position
+		r = random.random()*math.radians(180) #rotation
+		planetAndMoons = basenodes.Transform(
+			translation = p,
+			rotation = (0,1,0,r),
+			children = [
+				plan,
+				moons
+			]
+		)
+		planetAndMoons.name = name
+		planetAndMoons.sensititive = 0
+
+		#add t (planet) to solar sys
+		tmpHolder = self.uni.children[0].children[-1].children
+		tmpHolder.append(planetAndMoons)
+		self.uni.children[0].children[-1].children = tmpHolder
+		#calc coors of all planets in solarsys
+		coordsHolder = []
+		for planet in self.uni.children[0].children[-1].children:
+			coordsHolder.append( planet.translation )
+		#put those calced points in the line object in the solarsys, and update length of line
+		self.uni.children[0].children[-1].children[0].children[0].geometry.coord.point = coordsHolder
+		self.uni.children[0].children[-1].children[0].children[0].geometry.coordIndex = range( 1,len(coordsHolder))
+		
+		
+	def findPlanet( self, objPath ):
+		"""helper function to find the planet in the scenegraph from a path"""
+		"""returns object you are looking for, or null if not found"""
+		print objPath
+		tmpSys = objPath[2].name
+		tmpPlanet = objPath[3].name
+		#find the solar system position
+		sysPos = -1
+		sysNum = 0
+		for sys in self.uni.children[0].children:
+			if sys.name == tmpSys:
+				sysPos = sysNum
+			sysNum += 1
+		if sysPos >= 0:			
+			#find planet in solar system
+			planetPos = -1
+			planetNum = 0
+			for planet in self.uni.children[0].children[sysPos].children:
+				if planet.name == tmpPlanet:
+					planetPos = planetNum
+				planetNum += 1
+			if planetPos >= 0:
+				print "found planet at:", planetPos
+				return self.uni.children[0].children[sysPos].children[planetPos]
+			else:
+				print "didn't find planet in system"
+				return () 
+		else:
+			print "did't find system"
+			return ()
+			
+		
+	def findSys( self, objPath ):
+		"""helper function to find solarsys in the scenegraph from a path"""
+		"""returns object you are looking for, or null if not found"""
+		print objPath
+		tmpSys = objPath[2].name
+		tmpPlanet = objPath[3].name
+		#find the solar system position
+		sysPos = -1
+		sysNum = 0
+		for sys in self.uni.children[0].children:
+			if sys.name == tmpSys:
+				sysPos = sysNum
+			sysNum += 1
+		if sysPos >= 0:
+			print "Found sys at: ", sysPos
+			return self.uni.children[0].children[sysPos]
+		else:
+			print "did't find system"
+			return ()
+		
+		
+	def renderDetail( self, obj, geom ):
+		"""takes in an object, switches out its geometry for detailed geometry"""
+		#make the detail geometry
+		t = basenodes.Transform(
+			translation = ( 0,0,0 ),
+			rotation = ( 0,0,0,0 ),
+			children = [ 
+				basenodes.Shape(
+					geometry = geom,
+					appearance = basenodes.Appearance(
+						material = basenodes.Material( emissiveColor = (.8,.8,.8) ),
+					),
+				),
+			],
+		)		
+		obj.children[0] = t
+
+		
+	def unRenderDetail( self, obj ):
+		"""puts the simple geometry back into the passes object"""
+		obj.children[0].children = [
+			basenodes.TouchSensor(),
+			basenodes.Shape(
+				geometry = basenodes.Box( size = (0.3,0.3,0.3) ),
+				appearance = basenodes.Appearance(
+					material = basenodes.Material( emissiveColor = (.8,.8,.8) ),
+				),
+			),
+		]
+		
+	def rotatePlanets( self, ang ):	
+		"""rotate all the planets"""
+		for s in range( len( self.uni.children[0].children )):
+			for p in range( 1, len( self.uni.children[0].children[s].children ) ):
+				#rotate planet
+				self.uni.children[0].children[s].children[p].children[0].rotation = ( 0,1,0,ang )
+				#rotate all moons
+				for m in self.uni.children[0].children[s].children[p].children[1].children:
+					rotHolder = m.children[0].rotation
+					rotHolder[3] = ang * m.name
+					m.children[0].rotation = rotHolder
+		
+						
+	def rotateMoons( self, ang ):
+		"""rotates the moons on all the planets"""
+		
+	
+		
+		
